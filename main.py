392559496de586e96e82b64d103859e1a5a951a0
popstar403 <colinbroyles@gmail.com>
2024-10-28 16:07:08 -0500

    added class, __init__, __string__

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+class bankAccount():
+    def __init__(self, accountHolder, startingBalance = 0.0):
+        self.accountHolder = accountHolder
+        self.balance = startingBalance
+    
+    def __str__(self):
+        return f"Account owned by {self.accountHolder} with ${self.balance} in it"
